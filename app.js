9fe7ad405b9120d3e7ddc3640ff1f85310006477
Flow-flex-the-complex <fth29@hi.is>
2019-01-22 14:45:16 +0000

    dd

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-/* todo, setja upp express */
+const express = require('express');
+const path = require('path');
+const lectures = require('./lectures.js');
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
+
+ //Use EJS
+ app.set('view engine', 'ejs');
+ app.set('views', path.join(__dirname, 'views'));
+
+ app.get('/', (req, res) => {
+    res.render('index', { title: 'Forsíða' });
+  });
+  
+ app.use(express.static(path.join(__dirname, 'public')));
+
+
+ function notFoundHandler(req, res, next) { /* eslint-disable-line */
+    res.status(404).send('404 Not Found');
+  }
+  
+  function errorHandler(err, req, res, next) { /* eslint-disable-line */
+    console.error(err);
+    res.status(500).send('Villa!');
+  }
+  
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+ //Lecturelist
+ app.use('/', lectures);
+ //app.use('/html', lectures);
+ //app.use('/css', lectures);
+ //app.use('/js', lectures);
+
+ //start server
+ app.listen(port, () => {
+     console.info('Server running at http://${hostname}:${port}/');
+ });/* todo, setja upp express */
--- lectures.js
+++ lectures.js
@@ -1,20 +1,42 @@
 const express = require('express');
+const util = require('util');
+const fs = require('fs');
 
 const router = express.Router();
+
+const readFilePromise = util.promisify(fs.readFile);
 
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+//bua til fall sem les lectures.json af disk (async)
+async function lesaJSON() {
+  const file = await readFile('./lectures.json');
+
+  const json = JSON.parse(file)
+
+  return json;
+}
+
 async function list(req, res) {
-  /* todo útfæra */
+  const title = 'Fyrirlestrar';
+  const data = await lesaJSON;
+  const { lectures } = data;
+
+  res.render('lectures', (title, lectures))
+  //res.send('lectures');
 }
 
 async function lecture(req, res, next) {
   /* todo útfæra */
+  //Lesa fyrirlestrana inn og birta.
 }
-
+ 
 router.get('/', catchErrors(list));
+router.get('/html', catchErrors(list));
+router.get('/css', catchErrors(list));
+router.get('/js', catchErrors(list));
 router.get('/:slug', catchErrors(lecture));
 
 module.exports = router;
