f470d4c9fdf8e9d5bc1d0041203ccf4e40c9d8f6
Flow-flex-the-complex <fth29@hi.is>
2019-01-23 21:11:30 +0000

    Svo mikið!

--- app.js
+++ app.js
@@ -5,36 +5,38 @@ const app = express();
 const port = 3000;
 const hostname = '127.0.0.1';
 
- //Use EJS
- app.set('view engine', 'ejs');
- app.set('views', path.join(__dirname, 'views'));
+//Use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
- app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', lectures);
+
+app.get('/', (req, res) => {
     res.render('index', { title: 'Forsíða' });
   });
   
- app.use(express.static(path.join(__dirname, 'public')));
-
-
+ 
+ 
  function notFoundHandler(req, res, next) { /* eslint-disable-line */
-    res.status(404).send('404 Not Found');
-  }
-  
-  function errorHandler(err, req, res, next) { /* eslint-disable-line */
-    console.error(err);
-    res.status(500).send('Villa!');
-  }
-  
+  const title = 'Fannst ekki';
+  const message = 'Ó nei, efnið finnst ekki!';
+  res.status(404).render('error', { title, message });
+}
+
+function errorHandler(err, req, res, next) { /* eslint-disable-line */
+  const title = 'Villa kom upp';
+  const message = '';
+  res.status(500).render('error', { title, message })
+}
+
+
   app.use(notFoundHandler);
   app.use(errorHandler);
 
  //Lecturelist
- app.use('/', lectures);
- //app.use('/html', lectures);
- //app.use('/css', lectures);
- //app.use('/js', lectures);
 
  //start server
  app.listen(port, () => {
-     console.info('Server running at http://${hostname}:${port}/');
- });/* todo, setja upp express */
+     console.info(`Server running at http://${hostname}:${port}/`);
+ });
--- lectures.js
+++ lectures.js
@@ -1,42 +1,56 @@
 const express = require('express');
 const util = require('util');
 const fs = require('fs');
+const lectures = require('./lectures.json');
+const items = require('./items.js');
 
 const router = express.Router();
 
-const readFilePromise = util.promisify(fs.readFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
 //bua til fall sem les lectures.json af disk (async)
+//@returns {Promise}
 async function lesaJSON() {
-  const file = await readFile('./lectures.json');
-
-  const json = JSON.parse(file)
-
-  return json;
+  file = await readFileAsync('./lectures.json');
+  return JSON.parse(file);
+  
 }
 
 async function list(req, res) {
   const title = 'Fyrirlestrar';
-  const data = await lesaJSON;
-  const { lectures } = data;
+  const data = await lesaJSON();
 
-  res.render('lectures', (title, lectures))
-  //res.send('lectures');
+  res.render('index', {title, lectures : data.lectures});
 }
 
 async function lecture(req, res, next) {
   /* todo útfæra */
+  const { slug } = req.params;
+  const lectures = await lesaJSON();
+
+  const foundLecture = lectures.lectures.find(a => a.slug === slug);
+
+  if (!foundLecture) {
+    // hér væri líka hægt að skila 404 þar sem við erum að meðhöndla allt
+    // með `:/slug`
+    return next();
+  }
+
+  const { title } = foundLecture;
+  const { category } = foundLecture;
+  const html = items.createContent(foundLecture.content);
+
+
+  res.render('lecture', { title, html, category, lecture: foundLecture });
   //Lesa fyrirlestrana inn og birta.
 }
  
-router.get('/', catchErrors(list));
-router.get('/html', catchErrors(list));
-router.get('/css', catchErrors(list));
-router.get('/js', catchErrors(list));
 router.get('/:slug', catchErrors(lecture));
+router.get('/', catchErrors(list));
+
 
 module.exports = router;
